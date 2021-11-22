4ccd507f81aec1f2e0de837527cee1e4ce7bfe6f
Pannu <thepannu786@gmail.com>
2021-11-22 20:19:24 +0530

    set-up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import Todo from './components/Todo';
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo />
-      <hr />
-      <Todo text='this is second todo' />
-      <hr />
-      <Todo text='this is 3rd lulu' />
-      <hr /> <Todo text='nd so on' />
-
-  
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
