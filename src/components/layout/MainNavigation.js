2174352f1799957586b5bb8d92d6be3b7e85197e
Pannu <thepannu786@gmail.com>
2021-12-01 20:31:16 +0530

    Added fetch and adding data to firebase API

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
   return (
-    <header>
-      <div>Yo Yo Yo</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>Yo Yo Yo</div>
       <nav>
-        <ol>
+        <ul>
           <li>
             <Link to='/'>All Meetups</Link>
           </li>
@@ -16,8 +16,7 @@ function MainNavigation() {
           <li>
             <Link to='/favorites'>My Favorites</Link>
           </li>
-          
-        </ol>
+        </ul>
       </nav>
     </header>
   );
