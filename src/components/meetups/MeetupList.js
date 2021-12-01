61c1e8258131a2903ef6c7e2d8a9e66a035090f2
Pannu <thepannu786@gmail.com>
2021-12-02 01:32:44 +0530

    well sort of dne with a minor error.

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -1,20 +1,21 @@
-import MeetupItems from './MeetupItems';
+import MeetupItem from './MeetupItem';
 import classes from './MeetupList.module.css';
 
 function MeetupList(props) {
   return (
     <ul className={classes.list}>
       {props.meetups.map((meetup) => (
-        <MeetupItems
+        <MeetupItem
           key={meetup.id}
           id={meetup.id}
           image={meetup.image}
           title={meetup.title}
-          address={meetup.addresses}
+          address={meetup.address}
           description={meetup.description}
         />
       ))}
     </ul>
   );
 }
+
 export default MeetupList;
